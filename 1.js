e07e93aba4f638a4113fa5e644bf6974bee24ed7
anisakartikasari <03anisaks@gmail.com>
2022-02-02 22:26:58 -0800

    tugas

---
+++ 1.js
@@ -0,0 +1,7 @@
+function areaTriangel(a, t) {
+    console.log((1 / 2) * a * t);
+}
+
+areaTriangel (8, 10);
+areaTriangel (5, 7);
+areaTriangel (9, 2);
---
+++ 3.js
@@ -0,0 +1,10 @@
+function comp(firsString, lastString){
+    if(firsString.length == lastString.length){
+        console.log("true")
+    } else
+    console.log("false")
+}
+
+comp ("AB", "CD");
+comp ("ABC", "DE");
+comp ("hello", "edabit");
